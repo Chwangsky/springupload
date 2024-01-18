f83bda9110e43847f3fbf2bb50a9a029440787ea
Chwangsky <siztheday25@gmail.com>
2024-01-18 21:08:07 +0900

    파일 업로드 예제

---
+++ src/main/java/hello/upload/domain/UploadFile.java
@@ -0,0 +1,11 @@
+package hello.upload.domain;
+
+import lombok.Data;
+
+@Data
+public class UploadFile {
+
+    private final String uploadFileName;
+    private final String storeFileName;
+
+}
---
+++ src/main/java/hello/upload/file/FileStore.java
@@ -0,0 +1,63 @@
+package hello.upload.file;
+
+import hello.upload.domain.UploadFile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class FileStore {
+
+    @Value("${file.dir}")
+    private String fileDir;
+    public String getFullPath(String filename) {
+        return fileDir + filename;
+    }
+
+    public List<UploadFile> storeFiles(List<MultipartFile> multipartFiles) throws IOException {
+        List<UploadFile> storeFileResult = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+            if (!multipartFiles.isEmpty()) {
+                storeFileResult.add(storeFile(multipartFile));
+            }
+        }
+        return storeFileResult;
+    }
+
+    public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
+        if (multipartFile.isEmpty()) return null;
+
+        String originalFilename = multipartFile.getOriginalFilename();
+        // image.png
+
+        String storeFileName = createStoreFileName(originalFilename);
+
+        multipartFile.transferTo(new File(getFullPath(storeFileName)));
+
+        return new UploadFile(originalFilename, storeFileName);
+
+
+    }
+
+    private static String createStoreFileName(String originalFilename) {
+        // 서버에 저장하는 파일명
+        String uuid = UUID.randomUUID().toString();
+        // UUID에 확장자를 마지막에 붙여서 파일을 저장하고 싶음
+        String ext = extractExt(originalFilename);
+        String storeFileName = uuid + "." + ext;
+        return storeFileName;
+    }
+
+    // 운영상 편의 위함
+    private static String extractExt(String originalFilename) {
+        int pos = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(pos + 1);
+    }
+
+}
